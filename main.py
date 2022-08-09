26310e8edc9b1af4042cb025cf628716c37bcf60
BenjaminKlenner <benjamin.klenner@student.hhs.school.nz>
2022-08-09 22:11:40 +1200

    Authorization and basic database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from website import create_app
 
 app = create_app()
+app.config['SECRET_KEY'] = 'KlEnNeR'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Minute
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        minute = request.form.get('minute')
+
+        if len(minute) < 1:
+            flash('Minute is too short', category='error')
+        else:
+            new_minute = Minute(data=minute, user_id=current_user.id)
+            db.session.add(new_minute)
+            db.session.commit()
+            flash('Minute submitted!', category="success")
+
+    return render_template("home.html", user=current_user)
+
+@views.route('/delte-minute', methods=['POST'])
+def delete_minute():
+    minute = json.loads(request.data)
+    minuteId = minute['minuteId']
+    minute = Minute.query.get(minuteId)
+    if minute:
+        if minute.user_id == current_user.id:
+            db.session.delete(minute)
+            db.session.commit()
+
+    return jsonify({})
+
